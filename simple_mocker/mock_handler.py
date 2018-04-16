2b25b54ef9cacf17aa834bc8b588eb85dd5c6bf0
Ben Taljaard <ben.taljaard@devoteam.com>
2018-04-16 16:04:52 +0200

    refactor post

--- simple_mocker/mock_handler.py
+++ simple_mocker/mock_handler.py
@@ -55,6 +55,10 @@ class MockHandler(RequestHandler):
         else:
             for mock in mocks:
                 if self.request.uri == mock['mock']['request']['url']:
+                    if 'code' in mock['mock']['response']:
+                        local = dict()
+                        exec(mock['mock']['response']['code'],local)
+
                     #set http response code
                     self.set_status(mock['mock']['response']['status'])
 
@@ -64,7 +68,13 @@ class MockHandler(RequestHandler):
                         self.set_header(headerName, headerValue)
 
                     #return mock response
-                    self.write(mock['mock']['response']['body'])
+                    if 'render' in mock['mock']['response']:
+                        if mock['mock']['response']['render']:
+                            self.write(pystache.render(mock['mock']['response']['body'],local[mock['mock']['response']['renderdict']]))
+                        else:
+                            self.write(mock['mock']['response']['body'])
+                    else:
+                        self.write(mock['mock']['response']['body'])
 
                 #save request for assertions
                 # if mock['mock']['request']['body']:
